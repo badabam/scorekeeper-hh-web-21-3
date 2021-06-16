513503d9d41c93a7e42f3446b4b04b3062e47896
Jeremias Erbs <jerry@badabam.de>
2021-06-16 11:06:41 +0200

    Cleanup

--- src/components/Navigation.spec.js
+++ src/components/Navigation.spec.js
@@ -1,9 +1,6 @@
 import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 import Navigation from './Navigation'
 import { MemoryRouter as Router } from 'react-router-dom'
-
-const noop = () => {}
 
 describe('Navigation', () => {
   it('renders two NavLinks', async () => {
